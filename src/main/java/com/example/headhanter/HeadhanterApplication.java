571ed89f12a604b712250950e1c92cd521ff3263
Joodar <joodartynystanov@gmail.com>
2026-07-09 14:35:41 +0600

    Initial commit

---
+++ src/main/java/com/example/headhanter/HeadhanterApplication.java
@@ -0,0 +1,13 @@
+package com.example.headhanter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadhanterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadhanterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/headhanter/HeadhanterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.headhanter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadhanterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
