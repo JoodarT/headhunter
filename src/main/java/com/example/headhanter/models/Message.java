6d784efee6d9ff7393eb69c2d2a91c51d0d09224
Joodar <joodartynystanov@gmail.com>
2026-07-10 11:41:34 +0600

    Message

---
+++ src/main/java/com/example/headhanter/models/Message.java
@@ -0,0 +1,26 @@
+package com.example.headhanter.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "responded_applicants_id", nullable = false)
+    private RespondedApplicant respondedApplicant;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime timestamp;
+}
