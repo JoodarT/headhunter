5393dc58043a9e409ab7366b2053a7599a3ff526
Joodar <joodartynystanov@gmail.com>
2026-07-10 11:56:53 +0600

    User добавил анотации БД и измн логику

--- src/main/java/com/example/headhanter/models/User.java
+++ src/main/java/com/example/headhanter/models/User.java
@@ -1,28 +1,39 @@
 package com.example.headhanter.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-
 public class User {
 
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
 
-   private long id;
    private String name;
-   private String sureName;
+   private String surname;
    private Integer age;
+
+   @Column(unique = true, nullable = false)
    private String email;
+
+   @Column(nullable = false)
    private String password;
+
+   @Column(name = "phone_number")
    private String phoneNumber;
+
    private String avatar;
 
+   @Enumerated(EnumType.STRING)
+   @Column(name = "account_type", nullable = false)
    private AccountType accountType;
-
-
 }
