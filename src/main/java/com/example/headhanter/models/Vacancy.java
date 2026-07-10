21932264b3534618182462d381b4cff77dc36f97
Joodar <joodartynystanov@gmail.com>
2026-07-10 11:53:52 +0600

    UPDATE VACANCY добавил столб

--- src/main/java/com/example/headhanter/models/Vacancy.java
+++ src/main/java/com/example/headhanter/models/Vacancy.java
@@ -1,49 +1,47 @@
 package com.example.headhanter.models;
 
-
-
-import jdk.jfr.Category;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
-
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
 
-
-@Slf4j
+@Entity
+@Table(name = "vacancies")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vacancy {
-    public Vacancy(String name, long id, String description,
-                   Double salary, Integer expFrom, Integer expTo, Boolean isActive,
-                   LocalDateTime createdDate, LocalDateTime updateTime, User author) {
-        this.name = name;
-        this.id = id;
-        this.description = description;
-        this.salary = salary;
-        this.expFrom = expFrom;
-        this.expTo = expTo;
-        this.createdDate = createdDate;
-        this.updateTime = updateTime;
 
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
-    private long id;
 
+    @Column(columnDefinition = "TEXT")
     private String description;
+
     private Double salary;
 
+    @Column(name = "exp_from")
     private Integer expFrom;
+
+    @Column(name = "exp_to")
     private Integer expTo;
+
+    @Column(name = "is_active")
     private Boolean isActive;
 
+    @Column(name = "created_date")
     private LocalDateTime createdDate;
+
+    @Column(name = "update_time")
     private LocalDateTime updateTime;
 
-
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
-
-
+    @ManyToOne
+    @JoinColumn(name = "author_id")
     private User author;
-
-
 }
