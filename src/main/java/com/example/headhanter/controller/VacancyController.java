3b111f8e683e25a00512d9bdb4f377dd19189967
Joodar <joodartynystanov@gmail.com>
2026-07-11 16:24:57 +0600

    Modified

--- src/main/java/com/example/headhanter/controller/VacancyController.java
+++ src/main/java/com/example/headhanter/controller/VacancyController.java
@@ -7,17 +7,27 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
-@AllArgsConstructor
+
+@RestController
+@RequestMapping("/vacancy")
+
 public class VacancyController {
 
     private final VacancyService vacancyService;
 
+    @Autowired
+    public VacancyController(VacancyService vacancyService){
+        this.vacancyService = vacancyService;
+    }
 
-    public
-
-
+    @GetMapping("/demo")
+    public Vacancy getDemoVacancy() {
+        return vacancyService.createDemoVacancy();
+    }
 }
