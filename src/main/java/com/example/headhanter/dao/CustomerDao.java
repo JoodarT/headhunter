b0cab539662c4b0de022bb87ab761a1ea27b471b
Joodar <joodartynystanov@gmail.com>
2026-07-18 00:38:01 +0600

    add CustomerDao

---
+++ src/main/java/com/example/headhanter/dao/CustomerDao.java
@@ -0,0 +1,20 @@
+package com.example.headhanter.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class CustomerDao {
+    private Integer id;
+    private String name;
+    private String password;
+
+
+}
