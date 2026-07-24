bc06359587f690a22a0299a29ca4574e732d7be2
Joodar <joodartynystanov@gmail.com>
2026-07-25 00:13:54 +0600

    exeption

---
+++ src/main/java/com/example/headhanter/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+package com.example.headhanter.exception;
+
+import com.example.headhanter.dto.ErrorResponseDto;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorResponseDto> handleDuplicateKey(DuplicateKeyException ex) {
+        ErrorResponseDto error = new ErrorResponseDto(
+                "Конфликт данных",
+                List.of("Пользователь с таким email уже существует")
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+@ExceptionHandler(org.springframework.dao.DuplicateKeyException.class)
+public ResponseEntity<ErrorResponseDto> handleDuplicateKey(org.springframework.dao.DuplicateKeyException ex) {
+    ErrorResponseDto error = new ErrorResponseDto(
+            "Конфликт данных",
+            List.of("Пользователь с таким email уже существует")
+    );
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+}
