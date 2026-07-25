970bd756fadd9bedde434ddaa272f707b1d78da1
Joodar <joodartynystanov@gmail.com>
2026-07-25 16:54:20 +0600

    update SecurityConf

--- src/main/java/com/example/headhanter/config/SecurityConfig.java
+++ src/main/java/com/example/headhanter/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.example.headhanter.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,9 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -25,25 +21,19 @@ public class SecurityConfig {
 
     private final PasswordEncoder encoder;
 
-    private final DataSource dataSource;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable())); // Для корректного отображения H2-консоли
 
-    @Autowired
-    public void configGlobal(AuthenticationManagerBuilder authenticationManagerBuilder ) throws Exception {
-        authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource);
+        return http.build();
     }
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-//                        .anyRequest().permitAll()
-//                )
-//                .headers(headers -> headers.frameOptions(frame -> frame.disable())); // Для работы H2 Console
-//
-//        return http.build();
-//    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -51,14 +41,12 @@ public class SecurityConfig {
                 .username("admin")
                 .password(encoder.encode("123"))
                 .roles("ADMIN")
-                .authorities("FULL")
                 .build();
 
         UserDetails user = User.builder()
                 .username("user")
                 .password(encoder.encode("qwerty"))
                 .roles("USER")
-                .authorities("READ_ONLY")
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
