ea4c0750bc47dde1cf97d94a0a92d7c48d9efedf
Joodar <joodartynystanov@gmail.com>
2026-07-25 14:11:19 +0600

    securityConfig

---
+++ src/main/java/com/example/headhanter/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.headhanter.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final PasswordEncoder encoder;
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+//                        .anyRequest().permitAll()
+//                )
+//                .headers(headers -> headers.frameOptions(frame -> frame.disable())); // Для работы H2 Console
+//
+//        return http.build();
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("123"))
+                .roles("ADMIN")
+                .authorities("FULL")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(encoder.encode("qwerty"))
+                .roles("USER")
+                .authorities("READ_ONLY")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
