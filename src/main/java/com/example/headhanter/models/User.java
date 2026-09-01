aed689e22996769dc39294c399d7c369b87f567f
Joodar <joodartynystanov@gmail.com>
2026-09-02 05:16:46 +0600

    Вложенные списки Users

--- src/main/java/com/example/headhanter/models/User.java
+++ src/main/java/com/example/headhanter/models/User.java
@@ -3,6 +3,9 @@ package com.example.headhanter.models;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -40,5 +43,16 @@ public class User {
 
    @Column(name = "reset_password_token_expiry")
    private LocalDateTime resetPasswordTokenExpiry;
+
+   @OneToMany(mappedBy = "employer")
+   @Builder.Default
+
+   private List<Vacancy> vacancies =new ArrayList<>();
+
+   @OneToMany(mappedBy = "user")
+   @Builder.Default
+
+   private List<Resume> resumes =new ArrayList<>();
+
 
 }
